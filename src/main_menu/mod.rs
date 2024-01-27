1d9ab486e665a69c1b403f4818d4c5d8fb98e0bb
Vlad Gerasov <v1lammer@gmail.com>
2024-01-27 21:50:29 +0300

    Add main menu

---
+++ src/main_menu/mod.rs
@@ -0,0 +1,24 @@
+mod systems;
+
+use bevy::prelude::*;
+use systems::layout::{despawn_main_menu, spawn_main_menu};
+
+use crate::AppState;
+
+#[derive(Component)]
+pub struct MainMenu {}
+
+pub struct MainMenuPlugin;
+
+impl Plugin for MainMenuPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(
+            OnEnter(AppState::MainMenu),
+            spawn_main_menu.run_if(in_state(AppState::MainMenu)),
+        );
+        app.add_systems(
+            OnExit(AppState::MainMenu),
+            despawn_main_menu.run_if(in_state(AppState::Game)),
+        );
+    }
+}
---
+++ src/main_menu/systems/layout.rs
@@ -0,0 +1,46 @@
+use crate::main_menu::MainMenu;
+use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
+
+pub fn spawn_main_menu(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut window_query: Query<&mut Window, With<PrimaryWindow>>,
+) {
+    commands.spawn((
+        NodeBundle {
+            style: Style {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                ..default()
+            },
+            background_color: Color::PURPLE.into(),
+            ..default()
+        },
+        MainMenu {},
+    ));
+
+    let primary_window = window_query.single_mut();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(
+            primary_window.width() / 2.0,
+            primary_window.height() / 2.0,
+            0.0,
+        ),
+        ..default()
+    });
+}
+
+pub fn despawn_main_menu(
+    mut commands: Commands,
+    main_menu_query: Query<Entity, With<MainMenu>>,
+    camera_query: Query<Entity, With<Camera2d>>,
+) {
+    if let Ok(main_menu_entity) = main_menu_query.get_single() {
+        commands.entity(main_menu_entity).despawn();
+    }
+    if let Ok(camera_entity) = camera_query.get_single() {
+        commands.entity(camera_entity).despawn();
+    }
+}
